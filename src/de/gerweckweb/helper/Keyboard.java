ce82bed2ec858f50b6c59e26326b59439e7f0463
Daniel Neubieser <corataca@outlook.de>
2020-08-26 15:42:28 +0200

    insert Java doc and delete dead code

--- src/de/gerweckweb/helper/Keyboard.java
+++ src/de/gerweckweb/helper/Keyboard.java
@@ -5,31 +5,14 @@ import java.util.Scanner;
 
 public final class Keyboard {
 
-//	Keyboard() {
-//		input = new Scanner(System.in);
-//	}
-
-//	public int readInteger(String promptMsg, String errorMsg) {
-//		int num = 0;
-//		String strInput;
-//		boolean isValid = false;
-//		// keep looking until valid userinput
-//		while (isValid == false) {
-//			// prompt the user
-//			System.out.println(promptMsg);
-//			// grab input from Keyboard
-//			strInput = input.nextLine();
-//			// try to convert String to int
-//			try {
-//				num = Integer.parseInt(strInput);
-//				isValid = true;
-//			} catch (NumberFormatException e) {
-//				System.out.println(errorMsg);
-//			}
-//		}
-//		return num;
-//	}
-
+	/**
+	 *
+	 * @param promptMsg Output for request a specified Input
+	 * @param errorMsg Output tells user the errors
+	 * @param low determines the smallest number
+	 * @param high determines the largest number
+	 * @return returns the valid whole number entry
+	 */
 	public static int readInteger(String promptMsg, String errorMsg, int low, int high) {
 		Scanner input = new Scanner(System.in);
 		int num = 0;
@@ -59,31 +42,40 @@ public final class Keyboard {
 		return num;
 	}
 
-//	public double readDouble(String promptMsg, String errorMsg) {
-//		double num = 0.0;
-//		String strInput;
-//		boolean isValid = false;
-//		// keep looking until valid userinput
-//		while (isValid == false) {
-//			// prompt the user
-//			System.out.println(promptMsg);
-//			// grab input from Keyboard
-//			strInput = input.nextLine();
-//			// try to convert String to int
-//			try {
-//				num = Double.parseDouble(strInput);
-//				isValid = true;
-//			} catch (NumberFormatException e) {
-//				System.out.println(errorMsg);
-//			}
-//		}
-//		return num;
-//	}
+	/**
+	 *
+	 * @param promptMsg Output for request a specified Input
+	 * @param errorMsg Output tells user the errors
+	 * @return returns the valid whole number entry
+	 */
+	public static int readInteger(String promptMsg, String errorMsg) {
+		Scanner input = new Scanner(System.in);
+		int num = 0;
+		String strInput;
+		boolean isValid = false;
+		// keep looking until valid userinput
+		while (!isValid) {
+			// prompt the user
+			System.out.println(promptMsg);
+			// grab input from Keyboard
+			strInput = input.nextLine();
+			// try to convert String to int
+			try {
+				num = Integer.parseInt(strInput);
+			} catch (NumberFormatException e) {
+				System.out.println(errorMsg);
+			}
+			isValid = true;
+		}
+		return num;
+	}
 
 	/**
 	 *
 	 * @param textField check this textField for valid Input (Integer) between min, max
-	 * @return only Integer are possible
+	 * @param min determines the smallest number
+	 * @param max determines the largest number
+	 * @return only Integer are possible between min max
 	 */
 	public static int guiReadInt(JTextField textField, int min, int max){
 		// keep looking until valid userinput
